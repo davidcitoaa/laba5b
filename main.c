88176e8941a676249ca004dcfd65fb041e33e351
davidcitoaa <90219589+davidcitoaa@users.noreply.github.com>
2022-02-08 11:24:44 +0300

    refactoring / bug fix

--- main.c
+++ main.c
@@ -3,25 +3,63 @@
 #include "assert.h"
 #include "malloc.h"
 
+void rest_pushBackV_emptyVector() {
+    vectorVoid v = createVectorV(0, sizeof(char));
+    char a = 15;
+    pushBackV(&v, &a);
+    char b = 2;
+    pushBackV(&v, &b);
+    char c;
+    getVectorValueV(&v, 1, &c);
+
+    assert(b == c);
+    assert(v.size == 2);
+    assert(v.capacity == 2);
+    assert(c == 2);
+}
+
+void rest_pushBackV_fullVector() {
+    vectorVoid v = createVectorV(0, sizeof(char));
+    char a = 15;
+    pushBackV(&v, &a);
+    char b = 2;
+    pushBackV(&v, &b);
+    char c;
+    getVectorValueV(&v, 1, &c);
+
+    assert(b == c);
+    assert(v.size == 2);
+    assert(v.capacity == 2);
+    assert(c == 2);
+}
+
+void test_popBackV_notEmptyVector() {
+    vectorVoid v = createVectorV(0, sizeof(char));
+    int a = 5;
+    pushBackV(&v, &a);
+    int b = 6;
+    pushBackV(&v, &b);
+    popBackV(&v);
+    int c = 20;
+    pushBackV(&v, &c);
+    int d = 25;
+    pushBackV(&v, &d);
+    popBackV(&v);
+    int y;
+    getVectorValueV(&v, 0, &y);
+
+    assert(y == a);
+    assert(v.size == 2);
+    assert(v.capacity == 4);
+    assert(isEmptyV(&v));
+}
+
+void test() {
+    rest_pushBackV_emptyVector();
+}
 
 int main() {
-    size_t n;
-    scanf("%zd", &n);
-
-    vectorVoid v = createVectorV(0, sizeof(float));
-    for (int i = 0; i < n; i++) {
-        float x;
-        scanf("%f", &x);
-
-        pushBackV(&v, &x);
-    }
-
-    for (int i = 0; i < n; i++) {
-        float x;
-        getVectorValueV(&v, i, &x);
-
-        printf("%f ", x);
-    }
+    test();
 
     return 0;
 }
