c6ee68a78e48e8267393c7a1db5da8b4ed4a4b5b
davidcitoaa <90219589+davidcitoaa@users.noreply.github.com>
2022-02-14 19:13:49 +0300

    обмен строк и столбцов

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ int main() {
     matrix *a = getMemArrayOfMatrices(2, 2, 2);
     inputMatrices(a, 2);
 //    freeMemMatrix(m);
+    swapColumns(a[1], 1, 2);
     outputMatrices(a, 2);
 
     return 0;
